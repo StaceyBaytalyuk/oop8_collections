fc4bff6247b1ac0326b6b1a7e3b52132d128c568
Stacey Baytalyuk <stacey.baytalyuk@gmail.com>
2018-07-28 14:13:31 +0300

    first

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.run();
+    }
+
+    private void run(){
+        ProcessStuds s = new ProcessStuds();
+        s.menu();
+    }
+}
